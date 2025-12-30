213e427c00397af793c6167629a45273c11df15a
maxanarchia <88583018+massimofedrigo@users.noreply.github.com>
2025-12-30 21:44:20 +0100

    refactor: __init__.py updated

--- src/randomized_svd/__init__.py
+++ src/randomized_svd/__init__.py
@@ -1,7 +1,8 @@
 from .core import rsvd
 from .utils import optimal_threshold
 from .sklearn import RandomizedSVD
+from .pca import rpca
 
 __version__ = "0.3.0"
 
-__all__ = ["rsvd", "optimal_threshold", "RandomizedSVD"]
+__all__ = ["rsvd", "rpca", "optimal_threshold", "RandomizedSVD"]

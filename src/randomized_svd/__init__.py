81fd7cb178bf7bae1d76a49a52945f9eed0e69c7
Massimo Fedrigo <88583018+massimofedrigo@users.noreply.github.com>
2025-12-30 21:17:13 +0100

    Feat/scikit learn compatibility (#5)
    * feat: sklearn integration implemented
    
    * test: sklearn tests implemented
    
    * docs: sklearn integration example added
    
    * fix: removed unused import

--- src/randomized_svd/__init__.py
+++ src/randomized_svd/__init__.py
@@ -1,5 +1,7 @@
 from .core import rsvd
 from .utils import optimal_threshold
+from .sklearn import RandomizedSVD
 
+__version__ = "0.3.0"
 
-__all__ = ["rsvd", "optimal_threshold"]
+__all__ = ["rsvd", "optimal_threshold", "RandomizedSVD"]

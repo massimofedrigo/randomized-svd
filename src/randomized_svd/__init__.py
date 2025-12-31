71ed5a32ee6fad0d0fd4e72735210cb3e27963f4
maxanarchia <88583018+massimofedrigo@users.noreply.github.com>
2025-12-31 12:17:57 +0100

    chore: bump version to 0.4.0

--- src/randomized_svd/__init__.py
+++ src/randomized_svd/__init__.py
@@ -3,6 +3,6 @@ from .utils import optimal_threshold
 from .sklearn import RandomizedSVD
 from .pca import rpca
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 __all__ = ["rsvd", "rpca", "optimal_threshold", "RandomizedSVD"]
